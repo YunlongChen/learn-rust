53a0ca06b4d2c70169da4ab0da0b9e58547bcb20
Stan <1576302867@qq.com>
2024-12-29 01:08:49 +0800

    优化域名管理器的配置和帮助界面  添加测试数据库crate

---
+++ crates/test_db/queries/module_2.sql
@@ -0,0 +1,47 @@
+--! authors
+SELECT
+    *
+FROM
+    Author;
+
+--! books
+SELECT
+    Title
+FROM
+    Book;
+
+--! author_name_by_id
+SELECT
+    Author.Name
+FROM
+    Author
+WHERE
+    Author.Id = :id;
+
+--! author_name_starting_with AuthorNameStartingWithParams()
+SELECT
+    BookAuthor.AuthorId,
+    Author.Name,
+    BookAuthor.BookId,
+    Book.Title
+FROM
+    BookAuthor
+        INNER JOIN Author ON Author.id = BookAuthor.AuthorId
+        INNER JOIN Book ON Book.Id = BookAuthor.BookId
+WHERE
+    Author.Name LIKE CONCAT(:start_str::text, '%');
+
+--! select_voice_actor_with_character
+SELECT
+    voice_actor
+FROM
+    SpongeBobVoiceActor
+WHERE
+    character = :spongebob_character;
+
+--! select_translations
+SELECT
+    Title,
+    Translations
+FROM
+    Book;
